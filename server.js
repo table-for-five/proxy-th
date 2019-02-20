2ac73e28b81ab957a626e6cd8dbe8fad380850dc
Tingbin Huang <tingbinhuang@Tingbins-MacBook-Pro.local>
2019-02-19 23:52:43 -0800

    Refactor server.js with different port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3009;
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
